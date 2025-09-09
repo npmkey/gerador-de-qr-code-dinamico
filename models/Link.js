cdf93ea388f7e5c2af55b250a7d13b290ef4b36b
Keylla d'Afonseca <152008712+npmkey@users.noreply.github.com>
2025-09-09 12:59:36 -0300

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const LinkSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   url: { type: String, required: true },
-  expiresAt: { type: Date, required: true }
+  expiresAt: {
+    type: Date,
+    required: true,
+    index: { expires: 0 } // TTL: documento é removido automaticamente após expiresAt
+  }
 });
 
 module.exports = mongoose.model('Link', LinkSchema);
